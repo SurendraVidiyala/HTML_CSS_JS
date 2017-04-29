09b2c179d4e7ee6db3354acaa39ff8905e1eb010
surendra_vidiyala <vidiyala.surendra@gmail.com>
2017-04-28 23:04:15 -0500

    added variables about js

---
+++ JavaScript/variables.js
@@ -0,0 +1,9 @@
+var price1 = 5;
+var price2 = 6;
+var total = price1 + price2;
+
+document.getElementById("demo").innerHTML =
+    "The total is: " + total;
+var carName;
+carName = 'Volvo';
+document.getElementById('car').innerHTML = "The Car Name is:" + carName;
